dd4323e895888d1baebaab4260088b25f4628595
Mikhail Kantur <kami@slink.ws>
2020-02-29 13:20:41 +1000

    CI update

---
+++ .github/actions/build/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -euo pipefail
+
+echo "-----------------------------------------------------"
+echo "JARFILE : $INPUT_JARFILE"
+echo "ARTIFACT: $INPUT_ARTIFACT"
+echo "-----------------------------------------------------"
+
+./mvnw clean package && cd target && tar cvfz ${INPUT_ARTIFACT} ${INPUT_JARFILE}
